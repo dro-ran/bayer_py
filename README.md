198220151e6bf413effd69de55fa538f3806ccf3
dro-ran <13846494+dro-ran@users.noreply.github.com>
2024-05-26 12:00:52 +0900

    initial commit
    Signed-off-by: dro-ran <13846494+dro-ran@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tiny Python raw bayer image viewer
+
+## Usage
+```
+python bayer_py.py -i imx477_16bit_4056_3040.raw -j imx477.json
+```
+
+## Screenshot
+![screenshot](./screenshot.png "screenshot")
+
+## TODO
+8bit image support  
+more bayer pattern support  
+gain control for par channel  
+mosaic style preview  
+dng(exif) support  
+とか  
---
+++ bayer_py.py
@@ -0,0 +1,63 @@
+import sys
+import json
+import argparse
+import cv2
+import numpy as np
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_image', metavar='input raw image', type=str, help='This option is must need')
+    parser.add_argument('-j', '--input_json', metavar='input json config', type=str, help='This option is must need')
+
+    args, text = parser.parse_known_args()
+
+    if (not args.input_image or not args.input_json):
+        print('-i and -j option are must needed!')
+        sys.exit(1)
+
+    return args.input_image, args.input_json
+
+def json_parse(file_path):
+    with open(file_path, 'r') as f:
+        json_data = json.load(f)
+    return json_data
+
+def raw_process(image_path, config_json):
+    with open(image_path, 'rb') as raw_data:
+        raw_nda = np.fromfile(raw_data, np.uint16, config_json['width'] * config_json['height'])
+        # set signed 64bit for overflow support
+        raw_nda = np.array(raw_nda, dtype=np.int64)
+        # sub black and clip to 0
+        raw_nda - config_json['black_level']
+        raw_nda = np.clip(raw_nda, 0, None)
+
+        # dgain and clip to 65535
+        raw_nda *= config_json['dgain']
+        raw_nda = np.clip(raw_nda, None, 65535)
+        # go back to unsigned 16bit
+        raw_nda = np.array(raw_nda, dtype=np.uint16)
+
+        # reshape to image width and height
+        raw_nda = np.frombuffer(raw_nda, np.uint16).reshape(config_json['height'], config_json['width'])
+
+        # demosaic to BGR image
+        img_bgr = cv2.cvtColor(raw_nda, cv2.COLOR_BAYER_BGGR2BGR)
+        # img_gray = cv2.cvtColor(img, cv2.COLOR_BAYER_BGGR2GRAY)
+
+        # resizable window
+        cv2.namedWindow('demosaic', cv2.WINDOW_NORMAL)
+        cv2.imshow('demosaic', img_bgr)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+        return
+
+def main():
+    image_path, json_path = arg_parse()
+    config_json = json_parse(json_path)
+    raw_process(image_path, config_json)
+
+    return
+
+if __name__ == '__main__':
+    main()
